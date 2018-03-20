3471942f0fb8f68204f6165f4529714f31af3f8d
Gustav Andersson <gustav.k.andersson@gmail.com>
2018-03-21 00:33:53 +0100

    State variable lowpass filter brick

--- include/filter_bricks.h
+++ include/filter_bricks.h
@@ -45,6 +45,44 @@ protected:
     AudioBuffer     _audio_out;
 };
 
+
+/* State variable lowpass */
+class SVFFilterBrick : public DspBrick
+{
+public:
+    enum ControlInputs
+    {
+        CUTOFF = 0,
+        RESONANCE,
+        MAX_CONTROL_INPUTS,
+    };
+
+    enum AudioOutputs
+    {
+        FILTER_OUT = 0,
+        MAX_AUDIO_OUTS,
+    };
+
+    SVFFilterBrick(ControlPort cutoff, ControlPort resonance, AudioPort audio_in) :
+            _cutoff_ctrl(cutoff), _res_ctrl(resonance), _audio_in(audio_in) {}
+
+    const AudioBuffer& audio_output(int n) override
+    {
+        assert(n < MAX_AUDIO_OUTS);
+        return _audio_out;
+    }
+
+    void render() override;
+
+protected:
+    ControlPort     _cutoff_ctrl;
+    ControlPort     _res_ctrl;
+    AudioPort       _audio_in;
+    AudioBuffer     _audio_out;
+    std::array<float ,2> _reg{0,0};
+    ControlSmootherLinear _g_slew;
+};
+
 /* Standard Biquad with non-modulated filter parameters */
 class FixedFilterBrick : public DspBrick
 {
--- src/filter_bricks.cpp
+++ src/filter_bricks.cpp
@@ -49,7 +49,7 @@ void BiquadFilterBrick::render()
     for (unsigned int s = 0; s < PROC_BLOCK_SIZE; ++s)
     {
         std::array<float, 5> coeff;
-        for (unsigned int i = 0; i <= coeff.size(); ++i)
+        for (unsigned int i = 0; i < coeff.size(); ++i)
         {
             coeff[i] = _coeff[i].get();
         }
@@ -64,6 +64,28 @@ void BiquadFilterBrick::render()
     }
     _in = in;
     _out = out;
+}
+
+void SVFFilterBrick::render()
+{
+    /* From https://cytomic.com/files/dsp/SvfLinearTrapOptimised2.pdf */
+    const AudioBuffer& in = _audio_in.buffer();
+    float freq = 20 * powf(2.0f, _cutoff_ctrl.value() * 10.0f);
+    float k = 2 - 2 * _res_ctrl.value();
+    _g_slew.set(std::tan(static_cast<float>(M_PI) * freq / _samplerate));
+    for (unsigned int i = 0; i < PROC_BLOCK_SIZE; ++i)
+    {
+        float g = _g_slew.get();
+        float a1 = 1 / (1 + g * (g + k));
+        float a2 = g * a1;
+        float a3 = g * a2;
+        float v3 = in[i] - _reg[1];
+        float v1 = a1 * _reg[0] + a2 * v3;
+        float v2 = _reg[1] + a2 * _reg[0] + a3 * v3;
+        _reg[0] = 2.0f * v1 - _reg[0];
+        _reg[1] = 2.0f * v2 - _reg[1];
+        _audio_out[i] = v2;
+    }
 }
 
 void FixedFilterBrick::render()
@@ -82,7 +104,7 @@ void FixedFilterBrick::set_lowpass(float cutoff, float q)
 {
     /* Lowpass from http://www.musicdsp.org/files/Audio-EQ-Cookbook.txt */
     float res =  0.6f + 5.0f * q;
-    float w0 = 2.0f * M_PI * cutoff / _samplerate;
+    float w0 = 2.0f * static_cast<float>(M_PI) * cutoff / _samplerate;
     float w0_cos = std::cos(w0);
     float w0_sin = std::sin(w0);
     float alpha = w0_sin / res;
@@ -94,6 +116,8 @@ void FixedFilterBrick::set_lowpass(float cutoff, float q)
     _coeff[B0] = b0;
     _coeff[B1] = (1 - w0_cos) * norm;
     _coeff[B2] = b0;
-
+    _reg = {0, 0};
 }
+
+
 } // namepspace bricks
