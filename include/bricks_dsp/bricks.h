fb49b6d4a2b2bafbd1bd87032ca9e9e39e3bed64
Gustav Andersson <gustav.k.andersson@gmail.com>
2025-03-27 21:03:43 +0100

    Adding version and bumping to 1.0

--- include/bricks_dsp/bricks.h
+++ include/bricks_dsp/bricks.h
@@ -1,6 +1,11 @@
 #ifndef BRICKS_DSP_BRICKS_H
 #define BRICKS_DSP_BRICKS_H
 
+namespace bricks_dsp {
+constexpr int VERSION_MAJOR = BRICKS_DSP_VERSION_MAJOR;
+constexpr int VERSION_MINOR = BRICKS_DSP_VERSION_MINOR;
+}
+
 #include "dsp_brick.h"
 #include "envelope_bricks.h"
 #include "filter_bricks.h"
